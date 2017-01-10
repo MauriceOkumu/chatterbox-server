ed49d9e748c563a5d0f5fee4384e5f57004fd865
MauriceOkumu <ochiemaurice@yahoo.co.uk>
2017-01-09 20:26:14 -0800

    Seven tests passing

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,9 +12,10 @@ let headers = {
 let data = [];
 
 var requestHandler = function(request, response) {
+  
+  console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   let parsedURL = url.parse(request.url);
-  console.log(parsedURL);
 
   request.on('error', function(err) {
     console.error(err);
@@ -27,12 +28,15 @@ var requestHandler = function(request, response) {
   });
 
   if (parsedURL.pathname === '/classes/messages') {
+    response.setHeader('Content-Type', 'application/json');
+    
+    // Get request
     if (request.method === 'GET') {
       response.writeHead(200, headers);
-      response.write(JSON.stringify({results: [] }));
-    
+
+
+    // Post request
     } else if (request.method === 'POST') {
-      response.setHeader('Content-Type', 'text/json');
       response.writeHead(201, headers);
 
       let body = '';
@@ -42,57 +46,20 @@ var requestHandler = function(request, response) {
       });
 
       request.on('end', function() {
-        console.log(body);
-        response.end(body);
+        var obj = JSON.parse(body);
+        data.push(obj);
       });
 
       
-      // response.write(JSON.stringify({results: [] }));
     }
+    
+    response.write(JSON.stringify({results: data }));
     response.end();
+
   } else {
     response.statusCode = 404;
     response.end();
   }
-  
-  // let parsedURL = url.parse(request.url);
-  
-  // let statusCode = 404;
-
-  // if (parsedURL.pathname === '/classes/messages') {
-
-
-  //   headers['Content-Type'] = 'application/json';
-
-
-  //   if (request.method.toLowerCase() === 'get') {
-  //     statusCode = 200;
-  //   }
-
-  //   if (request.method.toLowerCase() === 'post') {
-  //     statusCode = 201;
-  //     let body = [];
-  //     request.on('data', function(chunk) {
-  //       body.push(chunk);
-  //     }).on('end', function() {
-  //       body = Buffer.concat(body).toString();
-  //     });
-  //     console.log(body);
-  //   }
-  // }
-
-  // if (statusCode === 404) {
-  //   response.writeHead(statusCode, headers);  
-  //   response.end();
-  // } else {
-  //   response.writeHead(statusCode, headers);
-  //   response.write(JSON.stringify({results: data }));
-  //   response.end();
-  // }
-
-  // console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
 };
-
 
 module.exports = requestHandler;
